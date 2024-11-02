e1ddc493e568cb186f585fc4ffe054c16af67279
chloepei867 <pei.zhiy@northeastern.edu>
2024-11-02 16:10:07 -0700

    made FAB button clickable and created new activity for adding contact

--- app/src/main/java/com/example/numad24fa_zhiyanpei/ContactsCollectorActivity.java
+++ app/src/main/java/com/example/numad24fa_zhiyanpei/ContactsCollectorActivity.java
@@ -1,5 +1,6 @@
 package com.example.numad24fa_zhiyanpei;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,21 +16,28 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsCollectorActivity extends AppCompatActivity {
 
+    FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contacts_collector);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.layout.activity_contacts_collector), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(ContactsCollectorActivity.this, AddContact.class);
+            startActivity(intent);
+        });
+
+
     }
 }
 
