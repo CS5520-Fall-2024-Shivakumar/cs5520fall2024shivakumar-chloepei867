7ebc3e956d620860baa6ed8885d6add8529670a3
chloepei867 <pei.zhiy@northeastern.edu>
2024-11-02 17:22:01 -0700

    created UI for adding contact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
 }
